7d5d189c5066b7e96bd608a6a624ded129ec2b6c
XUYH <603669825@qq.com>
2017-09-18 17:13:34 +0800

    增加XSS处理包

--- codelibrary/src/main/java/com/cn/xuyhproject/filter/XSSFilter.java
+++ codelibrary/src/main/java/com/cn/xuyhproject/filter/XSSFilter.java
@@ -17,7 +17,7 @@ public class XSSFilter implements Filter{
     @Override
     public void destroy() {
         // TODO Auto-generated method stub
-        filterConfig = null;
+        this.filterConfig = null;
     }
 
     @Override
@@ -30,6 +30,6 @@ public class XSSFilter implements Filter{
     @Override
     public void init(FilterConfig arg0) throws ServletException {
         // TODO Auto-generated method stub
-        filterConfig = arg0;
+        this.filterConfig = arg0;
     }
 }
